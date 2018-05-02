7458ed746e536068333db7b4bb6ce45d23b61b96
Sunny Aggarwal <sunnya97@gmail.com>
2018-05-01 21:20:59 -0400

    undid some stuff

--- x/auth/msgs.go
+++ x/auth/msgs.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 
@@ -9,49 +8,6 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
-
-// MsgClaimAccount - high level transaction of the auth module
-type MsgClaimAccount struct {
-	Address sdk.Address   `json:"address"`
-	PubKey  crypto.PubKey `json:"public_key"`
-}
-
-var _ sdk.Msg = MsgClaimAccount{}
-
-// NewMsgClaimAccount - msg to claim an account and set the PubKey
-func NewMsgClaimAccount(addr sdk.Address, pubkey crypto.PubKey) MsgClaimAccount {
-	return MsgClaimAccount{Address: addr, PubKey: pubkey}
-}
-
-// Implements Msg.
-func (msg MsgClaimAccount) Type() string { return "auth" }
-
-// Implements Msg.
-func (msg MsgClaimAccount) ValidateBasic() sdk.Error {
-	if bytes.Equal(msg.PubKey.Address(), msg.Address) {
-		return sdk.ErrInvalidPubKey(fmt.Sprintf("PubKey is invalid"))
-	}
-	return nil
-}
-
-// Implements Msg.
-func (msg MsgClaimAccount) Get(key interface{}) (value interface{}) {
-	return nil
-}
-
-// Implements Msg.
-func (msg MsgClaimAccount) GetSignBytes() []byte {
-	b, err := json.Marshal(msg) // XXX: ensure some canonical form
-	if err != nil {
-		panic(err)
-	}
-	return b
-}
-
-// Implements Msg.
-func (msg MsgClaimAccount) GetSigners() []sdk.Address {
-	return []sdk.Address{msg.Address}
-}
 
 // MsgChangeKey - high level transaction of the auth module
 type MsgChangeKey struct {
